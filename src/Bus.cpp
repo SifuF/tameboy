9125516bbcfcdf1e57ed410e2e1d37cc838cb29c
Francis Godden <francisgodden@hotmail.com>
2025-12-27 22:38:41 +0000

    Fix window position

--- src/Bus.cpp
+++ src/Bus.cpp
@@ -17,13 +17,13 @@ Bus::Bus() :
 
     readFile((char*)m_boot.get(), "../roms/DMG_ROM.bin");
     //readFile((char*)m_map.get(), "../roms/tetris.bin");
-    //readFile((char*)m_map.get(), "../roms/tetris_no_UpdateAudio.bin");
+    readFile((char*)m_map.get(), "../roms/tetris_no_UpdateAudio.bin");
     //readFile((char*)m_map.get(), "../roms/cpu_instrs.gb");
     //readFile((char*)m_map.get(), "../roms/tennis.bin");
     //readFile((char*)m_map.get(), "../roms/Alleyway.bin");
     //readFile((char*)m_map.get(), "../roms/dr.bin");
     //readFile((char*)m_map.get(), "../roms/spot.gb");
-    readFile((char*)m_map.get(), "../roms/taz.gb");
+    //readFile((char*)m_map.get(), "../roms/taz.gb");
 
     m_cpu.reset();
     compareLogo();
@@ -173,7 +173,7 @@ void Bus::write(uint16_t addr, uint8_t value)
         return;
     }
 
-    if (addr == 0xFF47) // Pallet
+    if (addr == 0xFF47) // Palette
         m_ppu.updatePaletteLookup(value);
 
     if (addr == 0xFF50)
--- src/PPU.cpp
+++ src/PPU.cpp
@@ -97,11 +97,11 @@ void PPU::drawLine(uint8_t LCDC, uint8_t SCX, uint8_t SCY, uint8_t WX, uint8_t W
     uint16_t tileMapAddr{};
     uint8_t scrollX{};
     uint8_t scrollY{};
-    //if (windowEnable && (LC >= WY)) { // TODO - window
-    if (false) {
+    if (windowEnable && (LC >= WY)) {
+    // if (false) {
         tileMapAddr = windowTileMapAddr;
         scrollX = WX - 7;
-        scrollY = LC - WY;
+        scrollY = -WY;
     }
     else {
         tileMapAddr = backgroundTileMapAddr;
